d1c4ad8d014489bfefc25381299e113824b6a34f
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2025-10-02 17:02:12 -0400

    script addded/file added

---
+++ 07-jsr-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("OG" "RRR" "KGF2")
+
+#indexes start from 0, size is 3
+
+echo "First movie: ${MOVIES[0]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Third movie: ${MOVIES[2]}"
+echo "All Movies: ${MOVIES[@]}"
