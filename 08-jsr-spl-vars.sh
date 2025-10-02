4fdef75ec5fb34ce87f36c83aa7051ba751a250d
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2025-10-02 17:32:18 -0400

    script addded/file added

--- 08-jsr-spl-vars.sh
+++ 08-jsr-spl-vars.sh
@@ -6,6 +6,9 @@ echo "First variable: $1"
 echo "Second variable: $2"
 echo "Third variable: $3"
 echo "Script name: $0"
+echo "which user executed the script: $USER"
+echo "Home directory of the user: $HOME"
+echo "Current working directory: $PWD"
 echo "Process ID of current script: $$"
 echo "Exit status of last command: $?"  # 0 means success, non-zero means failure
 sleep 60 &  # Sleep for 60 seconds in the background
