c87ea132a350c30f5fb7f9673a6c646fa0b9a77e
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2025-10-02 15:21:28 -0400

    script addded/file added

---
+++ 04-jsr-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1="$1"
+PERSON2="$2"
+
+echo "$PERSON1 : Hi $PERSON2"
+echo "$PERSON2 : Hi $PERSON1"
+echo "$PERSON1 : How are you?"
+echo "$PERSON2 : I am fine. What about you?"
+
+#tried person 1 as Rahim and now trying person 1 as Nikhil
