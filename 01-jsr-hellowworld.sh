fbd36391a1f4d218898f714b64c6cfe4125862b3
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2025-10-02 13:20:59 -0400

    hello-world script added

---
+++ 01-jsr-hellowworld.sh
@@ -0,0 +1,6 @@
+#!/bin/bash --> // It should be a comment in shell script. it is the interpret to execute the script. 
+
+#this line is commented
+
+echo "Hello World!"  # This line prints Hello World to the console
+# End of the script
