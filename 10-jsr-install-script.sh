ec9fd8dec35c22a1c127442e5d4ad49df8e08e18
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2025-10-02 21:02:37 -0400

    script addded/file added

---
+++ 10-jsr-install-script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#check if the user running the acript is root user or not
+# if root uder
+# allow him
+# elseshow error message and exit
+
+# run th einstallation commands
+# check if the installation was successful
+# if successful
+# show success message
+# else
+# show failure message
+
+$USER_ID=$(id -u)
+if[$USER_ID -ne 0]
+then
+  echo "ERROR: You must be root user to run this script"
+  exit 1
+fi
+
+dnf install mysql -y
+
+if[$? -ne 0]
+then
+  echo "ERROR: Installation failed"
+  exit 1
+else
+  echo "SUCCESS: Installation completed"
+fi
+
+dnf install git -y
+
+if[$? -ne 0]
+then
+  echo "ERROR: Installation failed"
+  exit 1
+else
+  echo "SUCCESS: Installation completed"
+fi
