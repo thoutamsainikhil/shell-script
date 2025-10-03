e1e3383d9baaa7db475f21a4bf52b4f6854a8339
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2025-10-03 12:27:40 -0400

    script addded/file added

---
+++ 13-jsr-logs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+RED="\e[31m"
+GREEN="\e[32m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+      if [ $? -ne 0 ]
+  then
+    echo -e "$RED ERROR: Installation failed"
+    exit 1
+  else
+  echo -e "$GREEN SUCCESS: Installation completed"
+  fi
+}
+
+echi "Script started executing at : $TIMESTAMP" &>>$LOG_FILE_NAME
+
+
+
+if [ $USER_ID -ne 0 ]
+then
+  echo -e "$RED ERROR: You must be root user to run this script"
+  exit 1
+fi
+
+dnf list installed mysql &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+  dnf install mysql -y &>>$LOG_FILE_NAME    
+  VALIDATE $? 
+else 
+  echo -e "$GREEN MySQL is already installed"
+fi
+
+
+dnf list installed git &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+  dnf install git -y &>>$LOG_FILE_NAME
+    VALIDATE $?
+else
+  echo -e "$GREEN Git is already installed"
+fi
+
