5dfaf58381c98136061e40ce208fc3add4b9b703
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2025-10-07 14:18:57 -0400

    script addded/file added

--- 16-jsr-backup.sh
+++ 16-jsr-backup.sh
@@ -9,9 +9,8 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14}
 
-LOGS_FOLDER="/home/ec2-user/shellscript-logs"
-mkdir -p $LOGS_FOLDER 
-LOG_FILE=$(echo $0 | cut -d "." -f1)
+LOGS_FOLDER=" -logs"
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
