c74aa60238cdb6fb7cce9fdb21afae074ff8236e
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2026-01-12 18:38:09 -0500

    Modified

--- 16-jsr-backup.sh
+++ 16-jsr-backup.sh
@@ -10,6 +10,7 @@ DEST_DIR=$2
 DAYS=${3:-14}
 
 LOGS_FOLDER="-logs"
+
 LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
